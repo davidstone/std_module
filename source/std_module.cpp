16a834f6be6ff0e20832045d6b0f84c46696d189
David Stone <davidfromonline@gmail.com>
2026-05-05 15:34:23 -0600

    Add `std::contiguous_iterator_tag` and `std::iterator_traits`

--- source/std_module.cpp
+++ source/std_module.cpp
@@ -232,6 +232,8 @@ export using std::input_iterator_tag;
 export using std::forward_iterator_tag;
 export using std::bidirectional_iterator_tag;
 export using std::random_access_iterator_tag;
+export using std::contiguous_iterator_tag;
+export using std::iterator_traits;
 export using std::reverse_iterator;
 export using std::make_reverse_iterator;
 export using std::default_sentinel_t;
